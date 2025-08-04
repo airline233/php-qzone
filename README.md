081aa1b115c11116109e3a5e269359f75f9e4f87
airline233 <airline233@gmail.com>
2025-08-04 16:28:38 +0800

    完善demo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # php-qzone
 PHP版本QQ空间的相关操作类
+
+看demo.php，懒得写文档了
--- demo.php
+++ demo.php
@@ -1,9 +1,15 @@
 <?php
 require 'qzone.class.php';
-$apiaddr = 'http://172.16.0.2:15000';
-$token = 'al233';
+$apiaddr = 'http://127.0.0.1:5000';
+$token = 'your_token_here';
 $instance = new qzone($apiaddr, $token);
 $imgfile = base64_encode(file_get_contents('test.jpg'));
-echo $instance -> upload($imgfile);
-
+$richval = $instance -> upload($imgfile);
+$tid = $instance -> publish('test11',1,$richval);
+$instance = new qzone($apiaddr, $token);
+$richval = $instance -> upload($imgfile,'base64','url');
+print_r($instance -> comment($tid,'测试带图评论1',1,$richval));
+print_r($instance -> comment($tid,'测试普通评论1'));
+print_r($instance -> comment($tid,'测试普通评论2'));
+print_r($instance -> delete($tid));
 ?>
