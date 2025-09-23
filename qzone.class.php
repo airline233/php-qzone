320f77e8b40a98fa3fba67257a9ed3e1b31d14f8
unknown <airline233@gmail.com>
2025-09-23 23:03:34 +0800

    修复无法删除的bug

--- qzone.class.php
+++ qzone.class.php
@@ -32,7 +32,6 @@ class qzone {
         $this -> pskey = $ckarr['p_skey'];
         $this -> token = $rt['data']['bkn'];
         $this -> HostUin = json_decode($this -> curl($apiaddr."/get_login_info?access_token=$actk"),1)['data']['user_id'];
-        return 1;
     }
 
     public function publish ($Content, $RichType = null, $Richval = null, $setTime = null, $ugcRight = 1, $allowUins = null) {
@@ -65,7 +64,8 @@ class qzone {
             'special_url' => null,
             'subrichtype' => null,
             'con' => $Content,
-            'feedversion' => '1&ver=1', //这俩应该都不动 写一起了 实际上是两个参数
+            'feedversion' => '1',
+            'ver' => '1', 
             'ugc_right' => $ugcRight, //权限 1为所有人可见 4为好友可见 64为仅自己可见
             'allow_uins' => $allowUins,
             'who' => (empty($allowUins)) ? null : 1,
@@ -147,7 +147,6 @@ class qzone {
             case 'url':
                 $url = $arr['data']['url'];
                 return $url;
-                break;
             case 'Richval':
                 $albumid = $arr['data']['albumid'];
                 $lloc = $arr['data']['lloc'];
@@ -156,91 +155,39 @@ class qzone {
                 $height = $arr['data']['height'];
                 $width = $arr['data']['width'];
                 return ",$albumid,$lloc,$sloc,$type,$height,$width,,$height,$width";
-                break;
             dafault:
                 return 'Invalid rtType';
-                break;
         }
     }
 
-    public function upvideo ($File, $desc = '') {
+    public function upvideo ($File) {
         /*
-            * 上传并发布视频
-            * File: 视频路径
-            * desc: 视频描述（说说的文本内容）
+            * 上传视频
+            * File： 视频路径
+            * 还未实现
         */
-        $albuminfo = $this -> curl("https://user.qzone.qq.com/proxy/domain/photo.qzone.qq.com/fcgi-bin/fcg_list_album_v3?g_tk={$this -> token}&callback=shine0_Callback&t=975885809&hostUin={$this -> HostUin}&uin={$this -> HostUin}&appid=4&inCharset=utf-8&outCharset=utf-8&source=qzone&plat=qzone&format=json&notice=0&filter=1&handset=4&pageNumModeSort=40&pageNumModeClass=15&needUserInfo=1&idcNum=4&callbackFun=shine0");
-        if(is_numeric($albuminfo)) return "Get Album Error!!";
-        /*$albumdata = json_decode($albuminfo,1)['data'];
-        if(!isset($albumdata)) $albumid = json_decode($this -> curl("https://user.qzone.qq.com/proxy/domain/photo.qzone.qq.com/cgi-bin/common/cgi_add_album_v2","inCharset=utf-8&outCharset=utf-8&hostUin={$this -> HostUin}&notice=0&callbackFun=_Callback&format=fs&plat=qzone&source=qzone&appid=4&uin={$this -> HostUin}&album_type=&birth_time=&degree_type=0&enroll_time=&albumname=Video&albumdesc=&albumclass=100&priv=1&question=&answer=&whiteList=&bitmap=10000000&qzreferrer=https%3A%2F%2Fuser.qzone.qq.com%2F{$this -> HostUin}%2Fmain"),1)['data']['album']['id']; //没有相册就创建相册 名称默认Video
-        else $albumid = $albumdata['albumListModeSort'][0]['id']; //有相册就默认取第一个
-        $albumname = $albumdata['albumListModeSort'][0]['name'] ?? 'Video';*/
-        //默认相册几乎都是仅自己可见，所以这边统一单独创建一个名为Video的相册
-        foreach (json_decode($albuminfo,1)['data']['albumListModeSort'] as $arr) if($arr['name'] == 'Video') $albumid = $arr['id']; 
-        if(!isset($albumid)) $albumid = json_decode($this -> curl("https://user.qzone.qq.com/proxy/domain/photo.qzone.qq.com/cgi-bin/common/cgi_add_album_v2?g_tk={$this -> token}","inCharset=utf-8&outCharset=utf-8&hostUin={$this -> HostUin}&notice=0&callbackFun=_Callback&format=json&plat=qzone&source=qzone&appid=4&uin={$this -> HostUin}&album_type=&birth_time=&degree_type=0&enroll_time=&albumname=Video&albumdesc=&albumclass=100&priv=1&question=&answer=&whiteList=&bitmap=10000000&qzreferrer=https%3A%2F%2Fuser.qzone.qq.com%2F{$this -> HostUin}%2Fmain"),1)['data']['album']['id']; //创建一个所有人可见 名为Video的相册
-        $albumname = 'Video';
         require_once('getid3/getid3.php');
         $binary = file_get_contents($File);
         $len = strlen($binary);
+        $num = ceil($len / 16384);
         $sha1 = sha1($binary);
         $getid3 = new getID3();
         $videotime = round($getid3 -> analyze($File)['playtime_seconds'] * 1000,2); //精确到0.01毫秒
         $time = time();
         $Params = "{\"control_req\":[{\"uin\":\"{$this -> HostUin}\",\"token\":{\"type\":4,\"data\":\"{$this -> pskey}\",\"appid\":5},\"appid\":\"video_qzone\",\"checksum\":\"{$sha1}\",\"check_type\":1,\"file_len\":{$len},\"env\":{\"refer\":\"qzone\",\"deviceInfo\":\"h5\"},\"model\":0,\"biz_req\":{\"sPicTitle\":\"upload.mp4\",\"sPicDesc\":\"\",\"sAlbumName\":\"\",\"sAlbumID\":\"\",\"iAlbumTypeID\":0,\"iBitmap\":0,\"iUploadType\":3,\"iUpPicType\":0,\"iBatchID\":0,\"sPicPath\":\"\",\"iPicWidth\":0,\"iPicHight\":0,\"iWaterType\":0,\"iDistinctUse\":0,\"sTitle\":\"upload\",\"sDesc\":\"\",\"iFlag\":0,\"iUploadTime\":{$time},\"iPlayTime\":{$videotime},\"sCoverUrl\":\"\",\"iIsNew\":111,\"iIsOriginalVideo\":0,\"iIsFormatF20\":0,\"extend_info\":{\"video_type\":\"3\",\"domainid\":\"5\"}},\"session\":\"\",\"asy_upload\":0,\"cmd\":\"FileUploadVideo\"}]}";
-        $response = $this -> curl("https://h5.qzone.qq.com/webapp/json/sliceUpload/FileBatchControl/{$sha1}?g_tk={$this -> token}",$Params, 1);
-        $starttime = strtotime($response[0]['Date']);
-        $rt_arr = json_decode($response[1],1);
+        $rt_arr = json_decode($this -> curl("https://h5.qzone.qq.com/webapp/json/sliceUpload/FileBatchControl/{$sha1}?g_tk={$this -> token}",$Params),1);
         $session = $rt_arr['data']['session'];
-        $slicesize = $rt_arr['data']['slice_size'];
-        $num = ceil($len / $slicesize);
 
+        $rt_arr = [];
         for($i=0;$i<$num;$i++) {
-            $offset = $i * $slicesize;
-            if($i+1 < $num) $end= $i * $slicesize;
+            $offset = $i * 16384;
+            if($i+1 < $num) $end= $i * 16384;
             else $end = $len;
-            $url = "https://h5.qzone.qq.com/webapp/json/sliceUpload/FileUploadVideo?seq={$i}&retry=0&offset={$offset}&end={$end}&total={$len}&type=json&g_tk={$this -> token}";
-            $base64 = base64_encode(substr($binary,$offset,$slicesize));
-            $Params = "{\"uin\":\"{$this -> HostUin}\",\"appid\":\"video_qzone\",\"session\":\"{$session}\",\"offset\":{$offset},\"data\":\"{$base64}\",\"checksum\":\"\",\"check_type\":1,\"retry\":0,\"seq\":{$i},\"end\":{$end},\"cmd\":\"FileUploadVideo\",\"slice_size\":{$slicesize},\"biz_req\":{}}";
-            $rt_arr = json_decode($this -> curl($url,$Params),1);
-            if(isset($rt_arr['data']['biz']['sVid'])) $sVid = $rt_arr['data']['biz']['sVid']; //这边用flag判断会有问题 我也不知道为什么 懒得调试了
-            if($rt_arr['ret'] != 0) return 'Upload Video File ERROR';
-        } //分片上传视频
-
-        $command = "ffmpeg -ss 00:00:01 -i $File -vframes 1 -f image2 {$File}.jpg";
-        @shell_exec($command); //生成视频缩略图
-        $binary = file_get_contents("{$File}.jpg");
-        if(!$binary) return "Generate JPEG File ERROR(Maybe permission denied or lack FFmpeg";
-        $len = strlen($binary);
-        $md5 = md5($binary); //卧槽傻逼一个字段有两种校验方式
-        $microtime = str_replace('.','',microtime(1));
-        $params = <<<json
-{"control_req":[{"uin":"{$this->HostUin}","token":{"type":4,"data":"{$this -> pskey}","appid":5},"appid":"pic_qzone","checksum":"{$md5}","check_type":0,"file_len":{$len},"env":{"refer":"huodong","deviceInfo":"h5"},"model":0,"biz_req":{"sPicTitle":"upload","sPicDesc":"","sAlbumName":"{$albumname}","sAlbumID":"{$albumid}","iAlbumTypeID":0,"iBitmap":0,"iUploadType":2,"iUpPicType":0,"iBatchID":{$microtime},"sPicPath":"","iPicWidth":0,"iPicHight":0,"iWaterType":0,"iDistinctUse":0,"mutliPicInfo":{"iBatUploadNum":1,"iCurUpload":0,"iSuccNum":0,"iFailNum":0},"iNeedFeeds":1,"iUploadTime":{$starttime},"stExtendInfo":{"mapParams":{"vid":"{$sVid}","photo_num":"undefined","video_num":"undefined"}},"stExternalMapExt":{"is_client_upload_cover":"1","is_pic_video_mix_feeds":"1"},"mapExt":{},"sExif_CameraMaker":"","sExif_CameraModel":"","sExif_Time":"","sExif_LatitudeRef":"","sExif_Latitude":"","sExif_LongitudeRef":"","sExif_Longitude":""},"session":"","asy_upload":0}]}
-json;
-        $rt_arr = json_decode($this -> curl("https://h5.qzone.qq.com/webapp/json/sliceUpload/FileBatchControl/{$md5}?g_tk={$this -> token}",$params),1);
-        $session = $rt_arr['data']['session'];
-        $slicesize = $rt_arr['data']['slice_size'];
-        $num = ceil($len / $slicesize);
-        for($i=0;$i<$num;$i++) {
-            $offset = $i * $slicesize;
-            if($i+1 < $num) $end= $i * $slicesize;
-            else $end = $len;
-            $url = "https://h5.qzone.qq.com/webapp/json/sliceUpload/FileUpload?seq={$i}&retry=0&offset={$offset}&end={$end}&total={$len}&type=json&g_tk={$this -> token}";
-            $base64 = base64_encode(substr($binary,$offset,$slicesize));
-            $Params = <<<json
-{"uin":"{$this -> HostUin}","appid":"pic_qzone","session":"{$session}","offset":{$offset},"data":"{$base64}","checksum":"","check_type":0,"retry":0,"seq":{$i},"end":{$end},"slice_size":{$slicesize},"biz_req":{"iUploadType":2}}
-json;
-            $rt_arr = json_decode($this -> curl($url,$Params),1);
-            if(isset($rt_arr['data']['biz']['sPhotoID'])) $sPhotoID = $rt_arr['data']['biz']['sPhotoID'];
-            if($rt_arr['ret'] != 0) return ['Upload JPEG File ERROR',$rt_arr, $url, $Params];
-        } //分片上传视频缩略图 我也不知道傻逼腾讯为什么要分片
-        @unlink("{$File}.jpg");
-        
-        if(isset($desc)) :
-            $rt_arr = json_decode($this -> curl("https://user.qzone.qq.com/proxy/domain/photo.qzone.qq.com/cgi-bin/common/cgi_modify_multipic_v2?g_tk={$this -> token}","qzreferrer=https%3A%2F%2Fuser.qzone.qq.com%2F{$this -> HostUin}&inCharset=utf-8&outCharset=utf-8&hostUin={$this -> HostUin}&notice=0&callbackFun=_Callback&format=json&plat=qzone&source=qzone&appid=4&uin={$this -> HostUin}&albumId={$albumid}&nvip=1&pub=1&albumTitle={$albumname}&albumDesc=&picCount=2&priv=1&afterUpload=1&total=1&modifyType=1&type=010&name=&desc=test11&tag=&codeList={$sPhotoID}?010??{$desc}??"),1);
-            if($rt_arr == 1) return 1;
-        endif;
-        if(isset($sPhotoID)) return 1;
-        return 0;
+            $url = "https://h5.qzone.qq.com/webapp/json/sliceUpload/FileUploadVideo?seq={$i}&retry=0&offset={$offset}&end={$end}&total=583937&type=json&g_tk={$this -> token}";
+            $base64 = base64_encode(substr($binary,$offset,16384));
+            $Params = "{\"uin\":\"{$this -> HostUin}\",\"appid\":\"video_qzone\",\"session\":\"{$session}\",\"offset\":{$offset},\"data\":\"{$base64}\",\"checksum\":\"\",\"check_type\":1,\"retry\":0,\"seq\":0,\"end\":16384,\"cmd\":\"FileUploadVideo\",\"slice_size\":16384,\"biz_req\":{}}";
+            $rt_arr[] = json_decode($this -> curl($url,$Params),1);
+        }
     }
 
     public function delete ($Tid) {
@@ -252,17 +199,16 @@ json;
         $postdata = array(
             'hostuin' => $this -> HostUin,
             'tid' => $Tid,
-            't1_source' => '1&code_version=1',
+            't1_source' => '1',
+            'code_version' => '1',
             'format' => 'json',
             'qzreferrer=https%3A%2F%2Fuser.qzone.qq.com%2F'. $this -> HostUin
         );
         $result = $this -> post('/emotion_cgi_delete_v6',$postdata);
-        if (is_numeric($result)) return array('code' => 0,'msg' => 'Req error Httpcode:'.$result); // 请求失败的话返回HTTP状态码
-        /*$result = '(' . $this -> cut("frameElement.callback(","</script>",$result);
-        $arr = json_decode($this -> cut("(",");",$result),1);*/ 
+        if (is_numeric($result)) return array('code' => 0,'msg' => 'Req error Httpcode:'.$result); // 请求失败则返回HTTP状态码
         $arr = json_decode($result,1);
-        if($arr['subcode'] == 0) return array('code' => 1); //成功时 subcode返回的是0，失败-200
-        return array('code' => 0);
+        if($arr['subcode'] == 0) return array('code' => 1); //成功时 subcode返回的是0，失败-200或其他
+        return array('code' => 0, 'msg' => $result);
     }
 
     public function comment ($Tid, $Content, $RichType = null, $Richval = null) {
@@ -296,10 +242,8 @@ json;
         );
         $result = $this -> post('/emotion_cgi_re_feeds',$postdata);
         if (is_numeric($result)) return array('code' => 0,'msg' => 'Req error Httpcode:'.$result); // 请求失败的话返回HTTP状态码
-        /*$result = '(' . $this -> cut("frameElement.callback(","</script>",$result);
-        $arr = json_decode($this -> cut("(",");",$result),1); 历史遗留 */
         $arr = json_decode($result,1);
-        if($arr['subcode'] == 0) return array('code' => 1); //成功时 subcode返回的是0，失败-800（也有可能是其他的）
+        if($arr['subcode'] == 0) return array('code' => 1); //成功时 subcode返回的是0，失败-800或其他
         return array('code' => 0,'msg' => $arr['message'],'subcode' => $arr['subcode']);
     }
 
@@ -314,17 +258,16 @@ json;
         if ($Type == 'user') $url = 'https://user.qzone.qq.com/proxy/domain/taotao.qzone.qq.com/cgi-bin'.$Path.'?g_tk='.$this -> token;
         elseif ($Type == 'upload') $url = 'https://up.qzone.qq.com/cgi-bin'.$Path.'?g_tk='.$this -> token;
         else return array('code' => 0,'msg' => 'Invalid Type');
-        $postdata = $Params;
-        if(is_array($Params)) 
-            //http_build_query($Params); //更优雅地处理post数据
-            //Update on 2025-09-09 这玩意会把&转义，但是我上面很多不变参都是写一起的，导致delete失效
-            foreach ($Params as $key => $value) $postdata .= "$key=".urlencode($value)."&";
+        $postdata = '';
+        if(is_array($Params)) $postdata = http_build_query($Params);
+        //foreach ($Params as $key => $value) $postdata .= "$key=".urlencode($value)."&";
+        else $postdata = $Params;
         $postdata = rtrim($postdata, '&');
         $result = $this -> curl($url, $postdata);
         return $result;
     }
 
-    private function curl($url, $data=null, $reheader = false) {
+    private function curl($url,$data=null) {
         /*
             * 发送curl请求
             * url: 请求的URL
@@ -333,7 +276,7 @@ json;
         */
         $ch = curl_init();
         $cu[CURLOPT_URL] = $url;
-        $cu[CURLOPT_HEADER] = $reheader;
+        $cu[CURLOPT_HEADER] = false;
         $cu[CURLOPT_RETURNTRANSFER] = true;
         $cu[CURLOPT_FOLLOWLOCATION] = true;
         if($data):
@@ -347,35 +290,13 @@ json;
         $cu[CURLOPT_USERAGENT] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0";
         $cu[CURLOPT_TIMEOUT] = "10";
         curl_setopt_array($ch, $cu);
-        $response = curl_exec($ch);
+        $content = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         if ($httpCode != 200) {
           return $httpCode;
         }
-        if ($reheader == 1):
-            $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-            $headerStr = substr($response, 0, $headerSize);
-            $body = substr($response, $headerSize);
-
-            // 6. 解析响应头字符串为数组
-            $headerRows = explode("\r\n", trim($headerStr));
-            $parsedHeaders = [];
-            foreach ($headerRows as $row) {
-                // 第一行是 HTTP 状态行，例如: HTTP/1.1 200 OK
-                if (strpos(strtolower($row), 'http/') === 0) {
-                    $parsedHeaders['status'] = $row;
-                } else if (!empty($row)) {
-                    $parts = explode(':', $row, 2);
-                    if (count($parts) === 2) {
-                        // 将其他头信息存为 key => value 格式
-                        $parsedHeaders[trim($parts[0])] = trim($parts[1]);
-                    }
-                }
-            }
-            $response = [$parsedHeaders,$body];
-        endif;
         curl_close($ch);
-        return $response;
+        return $content;
       }
     
       private function cut($begin,$end,$str){
